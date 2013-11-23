eba824cfe9a32999aa469619937dd71eddbb2c62
Anthony Bouch <tony@58bits.com>
2013-11-23 19:32:21 +0700

    Updated license and removed README.

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ get_required_input() {
     fi
 
     while true; do
-        echo -n $1 >&2
+        echo -n "$1" >&2
         read input
         if [ -z "$input" ]; then
             echo "$2" >&2
@@ -53,6 +53,36 @@ process_template() {
     done < $1
 }
 
+###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+#
+# FUNCTION: git_config
+#
+###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
+git_config() {
+
+    echo "Configuring ~/.gitconfig..."
+    name=$(get_required_input "Enter your full name: " "Please enter your full name.")
+    email=$(get_required_input "Enter your email address: " "Please enter your email address.")
+    github=$(get_required_input "Enter your github user ID: " "Please enter your github user ID.")
+    home=$HOME
+
+    echo "Create a new ${HOME}/.gitconfig file with name: $name, email: $email and github user ID: $github?"
+    echo -n "(y/n)"
+    read ans
+
+    case $ans in
+    Y|y) ;;
+    [Yy][Ee][Ss]) ;;
+    N|n) exit ;;
+    [Nn][Oo]) exit ;;
+    *) echo "Invalid command"
+       exit ;;
+    esac
+
+    echo "Creating ~/.gitconfig"
+    process_template "./gitconfig.template" "${HOME}/.gitconfig"
+}
+
 
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 # 
@@ -60,27 +90,15 @@ process_template() {
 #
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~##
 
-echo "Configuring ~/.gitconfig..."
-name=$(get_required_input "Enter your full name: " "Please enter your full name.")
-email=$(get_required_input "Enter your email address: " "Please enter your email address.")
-github=$(get_required_input "Enter your github user ID: " "Please enter your github user ID.")
-home=$HOME
+if [ ! -d "${HOME}/.dotfiles" ]; then
+    echo "Your 'dotfiles' source files and this script should be located in the directory ${HOME}/.dotfiles"
+    echo "Please clone the git repo into this directory and try again."
+    exit 1
+fi
 
-echo "Create a new ${HOME}/.gitconfig file with name: $name, email: $email and github user iD: $github?"
-echo -n "(y/n)"
-read ans
+cd $HOME/.dotfiles #Just to be sure.
 
-case $ans in
-Y|y) ;;
-[Yy][Ee][Ss]) ;;
-N|n) exit ;;
-[Nn][Oo]) exit ;;
-*) echo "Invalid command"
-   exit ;;
-esac
-
-echo "Creating ~/.gitconfig"
-process_template "./gitconfig.template" "${HOME}/.gitconfig"
+#git_config
 
 echo "Symlinking files"
 
