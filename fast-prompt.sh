48df63f1aac091826926501f0d2c1eb9818a41fd
Anthony Bouch <tony@58bits.com>
2018-05-01 22:26:45 +0700

    Turned on fast-prompt upstream git tracking

--- fast-prompt.sh
+++ fast-prompt.sh
@@ -11,8 +11,8 @@ export ENABLE_GIT_STATUS=true
 # export GIT_PS1_SHOWUNTRACKEDFILES=true
 # export GIT_PS1_SHOWCOLORHINTS=true
 # export GIT_PS1_HIDE_IF_PWD_IGNORED=true
-# export GIT_PS1_SHOWDIRTYSTATE=true 
-# export GIT_PS1_SHOWUPSTREAM="auto"
+export GIT_PS1_SHOWDIRTYSTATE=true 
+export GIT_PS1_SHOWUPSTREAM="auto"
 
 # Enable powerline symbols. You don't need to install powerline.
 # There is a sub-repo of modified powerline fonts in the
